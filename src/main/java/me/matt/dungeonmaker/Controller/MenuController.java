f30faabaeb92183aa3c9e586507bcf41389ff920
Your Name <worzalam02@gmail.com>
2018-05-06 19:56:08 -0400

    Added menu fxml. Not complete.

---
+++ src/main/java/me/matt/dungeonmaker/Controller/MenuController.java
@@ -0,0 +1,4 @@
+package me.matt.dungeonmaker.Controller;
+
+public class MenuController {
+}
--- src/main/java/me/matt/dungeonmaker/Main.java
+++ src/main/java/me/matt/dungeonmaker/Main.java
@@ -1,4 +1,24 @@
 package me.matt.dungeonmaker;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    //todo info needed to create the page.
+    //page length, number of buttons x/y, room attempts, minroomx, maxroomx, minroomy, maxroomy, repeat chance
+    //optional: colors of (wall, tunnel, room, finalroom, connector
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/menu.fxml"));
+        primaryStage.setTitle("Dungeon Maker Settings");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 }
